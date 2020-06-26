2357b32b4dcebe873894984717ca8949e4785d72
sunhyung2007 <54801031+sunhyung2007@users.noreply.github.com>
2020-06-26 17:05:33 +0900

    Update yearof2017.java

--- 통합본/MyApplication/app/src/main/java/com/example/myapplication/yearof2017.java
+++ 통합본/MyApplication/app/src/main/java/com/example/myapplication/yearof2017.java
@@ -3,7 +3,8 @@ package com.example.myapplication;
  * 작성자 : 2016039059 김선형
  * 수정일 : 2020.06.26
  * 프로그램명 : 입학연도 2017년 체크리스트
- ***************************************/
+ * 프로그램 설명 : 입학년도 2017년 체크리스트 뷰
+ **************************************/
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
